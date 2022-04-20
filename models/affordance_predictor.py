a18907dc885e4832281094170687160c623ebf34
Abdul Basit Anees <50206820+basitanees@users.noreply.github.com>
2022-04-20 19:29:35 +0300

    Update affordance_predictor.py

--- models/affordance_predictor.py
+++ models/affordance_predictor.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 
 
-from models.cilrs import FC
+from models.cilrs import FC, Branching
 
 class AffordancePredictor(nn.Module):
     """Afforance prediction network that takes images as input"""
@@ -9,23 +9,37 @@ class AffordancePredictor(nn.Module):
         super(AffordancePredictor, self).__init__()
         n_output = 1000
         self.resnet = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=True, num_classes=n_output)
-        self.affordances = FC(params={'neurons': [n_output, 256, 256, 4],
+#         self.affordances = FC(params={'neurons': [n_output, 256, 256, 2],
+#                                        'dropouts': [0.0, 0.5,0.0],
+#                                        'end_layer': True})
+        self.traffic_state = FC(params={'neurons': [n_output, 256, 256, 1],
                                        'dropouts': [0.0, 0.5,0.0],
                                        'end_layer': True})
-        self.activation = torch.nn.Sigmoid()
+        self.traffic_dist = FC(params={'neurons': [n_output, 256, 256, 1],
+                                       'dropouts': [0.0, 0.5,0.0],
+                                       'end_layer': True})
+        # Create the fc vector separatedely
+        branch_fc_vector = []
+        for i in range(4):
+            branch_fc_vector.append(FC(params={'neurons': [n_output, 256, 256] +
+                                                         [len(['route_angle','lane_dist'])],
+                                               'dropouts':  [0.0, 0.5, 0.0],
+                                               'end_layer': True}))
+
+        self.affordance_branches = Branching(branch_fc_vector)  # Here we set branching automatically
         
         for m in self.modules():
             if isinstance(m, nn.Linear):
                 nn.init.xavier_uniform_(m.weight)
                 nn.init.constant_(m.bias, 0.1)
-                                       
-
-    def forward(self, img):
+    
+    def forward(self, img, command):
         x = self.resnet(img)
-        affordances_output = self.affordances(x)
-        cont_affordance = affordances_output[:,:3]
-        disc_affordance = affordances_output[:,3:4]
-        disc_affordance = self.activation(disc_affordance)
-        return cont_affordance, disc_affordance
-        
-        
+        affordances_output = self.affordance_branches(x)
+        affordances_output = affordances_output[torch.arange(affordances_output.shape[0]),command,:]
+        tl_state = self.traffic_state(x)
+#         tl_state_off = (tl_state < 0)
+        tl_dist = self.traffic_dist(x)
+#         tl_dist[tl_state_off] = 45.0
+        cont_affordance = torch.cat([affordances_output[:,0:2], tl_dist],dim=1)
+        return cont_affordance, tl_state#.unsqueeze(-1)
