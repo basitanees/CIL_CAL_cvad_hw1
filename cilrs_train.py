27d993b105f1c86024bea616e93d43e870234d1e
Abdul Basit Anees <50206820+basitanees@users.noreply.github.com>
2022-04-20 19:31:08 +0300

    Update cilrs_train.py

--- cilrs_train.py
+++ cilrs_train.py
@@ -4,33 +4,34 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 # from tqdm.notebook import tqdm
 
-from expert_dataset import ExpertDataset
-from models.cilrs import CILRS
+# from expert_dataset import ExpertDataset
+# from models.cilrs import CILRS
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 
 
-def calc_loss(preds, batch):
+def calc_loss(preds, batch, loss_fn):
     """Calculate loss for a batch"""
-    branches, speed_pr = preds
-    action_pred = branches[torch.arange(branches.shape[0]),batch['command'],:] 
-    loss = 0.75*F.mse_loss(action_pred, batch['labels'])
-    loss += 0.25*F.mse_loss(speed_pr, batch['speed'])
-    return loss
+    action_pred, speed_pr = preds
+#     action_pred = branches[torch.arange(branches.shape[0]),batch['command'],:] 
+    loss = 1.5*loss_fn(action_pred[:,0:1], batch['labels'][:,0:1])
+    loss = loss_fn(action_pred[:,1:3], batch['labels'][:,1:3])
+    loss += loss_fn(speed_pr, batch['speed'])
+    return loss/3
 
-def validate(model, dataloader):
+def validate(model, dataloader, loss_fn):
     """Validate model performance on the validation dataset"""
     # Your code here
     total_loss = 0.0
     model.eval()
     for batch in dataloader:
-        preds = model(batch['rgb'], batch['speed'])
-        loss = calc_loss(preds, batch)
+        preds = model(batch['rgb'], batch['speed'], batch['command'])
+        loss = calc_loss(preds, batch, loss_fn)
         total_loss += loss.item()
     total_loss /= len(dataloader)
     return total_loss
 
-def train(model, dataloader, optim):
+def train(model, dataloader, optim, loss_fn):
     """Train model on the training dataset for one epoch"""
     # Your code here
 #     total_loss = 0.0
@@ -39,8 +40,8 @@ def train(model, dataloader, optim):
     with tqdm(dataloader, unit="batch") as tepoch:
         for batch in tepoch:
             optim.zero_grad()
-            preds = model(batch['rgb'], batch['speed'])
-            loss = calc_loss(preds, batch)
+            preds = model(batch['rgb'], batch['speed'], batch['command'])
+            loss = calc_loss(preds, batch, loss_fn)
             loss.backward()
             optim.step()
             tepoch.set_postfix(loss=loss.item())
@@ -61,35 +62,38 @@ def plot_losses(train_loss, val_loss):
     plt.savefig('losses.png')
 
 def main():
-    # Change these paths to the correct paths in your downloaded expert dataset
-    train_root = "/home/aanees20/My Data/Comp 523/Homework 1/expert_data/train"
-    val_root = "/home/aanees20/My Data/Comp 523/Homework 1/expert_data/val"
-    model = CILRS()
-    model = model.cuda()
-    train_dataset = ExpertDataset(train_root)
-    val_dataset = ExpertDataset(val_root)
+#     # Change these paths to the correct paths in your downloaded expert dataset
+train_root = "/userfiles/eozsuer16/expert_data/train"
+val_root = "/userfiles/eozsuer16/expert_data/val"
+model = CILRS()
+model = model.cuda()
+train_dataset = ExpertDataset(train_root)
+val_dataset = ExpertDataset(val_root)
 
-    # You can change these hyper parameters freely, and you can add more
-    num_epochs = 10
-    batch_size = 64
-    save_path = "cilrs_model.ckpt"
+# You can change these hyper parameters freely, and you can add more
+num_epochs = 12
+batch_size = 64
+save_path = "cilrs_model_no_act_weighted_512_mse.ckpt"
 
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
-                              drop_last=True)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
+                          drop_last=True)
+val_loader = DataLoader(val_dataset, batch_size=batch_size//2, shuffle=False)
 
-    params = model.parameters()
-    optim = torch.optim.Adam(params, lr=0.0002)
+params = model.parameters()
+optim = torch.optim.Adam(params, lr=0.00015)
+# loss_fn = nn.L1Loss()
+loss_fn = nn.MSELoss()
 
-    train_losses = []
-    val_losses = []
-    for i in range(num_epochs):
-        train_losses.append(train(model, train_loader, optim))
-        val_losses.append(validate(model, val_loader))
-        torch.save(model, save_path)
-        print(val_losses)
-    # torch.save(model, save_path)
-    plot_losses(train_losses, val_losses)
+train_losses = []
+val_losses = []
+for i in range(num_epochs):
+    train_losses.append(train(model, train_loader, optim, loss_fn))
+    val_losses.append(validate(model, val_loader, loss_fn))
+    print(val_losses)
+    torch.save(model, save_path)
+
+train_losses2 = sum(train_losses, [])
+plot_losses(train_losses2, val_losses)
 
 
 if __name__ == "__main__":
